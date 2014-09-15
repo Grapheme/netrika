1e70857c952262922d87f13b3815e4563aa24550
Владимир <vkharseev@gmail.com>
2014-09-15 15:43:35 +0400

    Настройка

--- app/config/github.php
+++ app/config/github.php
@@ -2,13 +2,13 @@
 
 return array(
     'active'            => TRUE,
-    'test_mode_key'     => 'OZiBhRszadigD4T5mGDoCdPfne2pzDNa',
+    'test_mode_key'     => 'S2IgteaZdojZPYppPPYikcfMJfZgrLSb',
     'post_data'         => array(),
     'git_path'          => '/usr/bin/',
     'remote'            => 'origin',
     'branch'            => 'master',
-    'repository_name'   => 'techvuz',
-    'repository_id'     => 23617622,
+    'repository_name'   => 'netrika',
+    'repository_id'     => 24047268,
     'user_group'        => 'www-data',
     'user_name'         => 'www-data',
     'set_log'           => TRUE,
