75f6f42ee695c5605834f0fba78069a84d93a788
Владимир <vkharseev@gmail.com>
2014-09-18 12:58:21 +0400

    Модерация вакансий

---
+++ app/config/dic/vacancies.php
@@ -0,0 +1,56 @@
+<?php
+
+return array(
+
+    'fields' => function () {
+
+        return array(
+            'description_vacancy' => array(
+                'title' => 'Описание вакансии',
+                'type' => 'textarea_redactor',
+            ),
+        );
+
+    },
+
+
+    'menus' => function($dic, $dicval = NULL) {
+        $menus = array();
+        return $menus;
+    },
+
+
+    'actions' => function($dic, $dicval) {
+    },
+
+    'hooks' => array(
+
+        'before_all' => function ($dic) {
+        },
+
+        'before_index' => function ($dic) {
+        },
+
+        'before_index_view' => function ($dic, $dicvals) {
+        },
+
+        'before_create_edit' => function ($dic) {
+        },
+        'before_create' => function ($dic) {
+        },
+        'before_edit' => function ($dic, $dicval) {
+        },
+
+        'before_store_update' => function ($dic) {
+        },
+        'before_store' => function ($dic) {
+        },
+        'before_update' => function ($dic, $dicval) {
+        },
+
+        'before_destroy' => function ($dic, $dicval) {
+        },
+    ),
+
+    'seo' => false,
+);
