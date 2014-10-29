7209dbe7b977a15a7f785dfd23e15b9cde3a25e4
KostyaD <thedamaxstudio@gmail.com>
2014-10-29 22:52:45 +0300

    footer columns

--- app/views/templates/site/footer.blade.php
+++ app/views/templates/site/footer.blade.php
@@ -57,20 +57,22 @@ foreach ($projects as $project) {
                     </ul>
                 </div>
                 <div class="grid_12 footer-line mobile-hidden"></div>
-                <div class="grid_4 grid_m12 mobile-hidden">
+                <div class="grid_12 footer-columns mobile-hidden">
 
                     @foreach ($solutions as $solution)
-                        <div class="footer-title">{{ $solution->name }}</div>
-                        <?
-                        $solution_projects = @$array[$solution->id];
-                        ?>
-                        @if (count($solution_projects))
-                            <ul class="footer-list">
-                            @foreach ($solution_projects as $project)
-                                <li><a href="{{ URL::route('project-one', $project->slug) }}">{{ $project->name }}</a>
-                            @endforeach
-                            </ul>
-                        @endif
+                        <div class="footer-lists">
+                            <div class="footer-title">{{ $solution->name }}</div>
+                            <?
+                            $solution_projects = @$array[$solution->id];
+                            ?>
+                            @if (count($solution_projects))
+                                <ul class="footer-list">
+                                @foreach ($solution_projects as $project)
+                                    <li><a href="{{ URL::route('project-one', $project->slug) }}">{{ $project->name }}</a>
+                                @endforeach
+                                </ul>
+                            @endif
+                        </div>
                     @endforeach
 
                 </div>
